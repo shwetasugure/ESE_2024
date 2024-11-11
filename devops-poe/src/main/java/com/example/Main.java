3064f197454bf98982101585f6765c00fa4332fe
Shweta Sugure <shwetasugure333@gmail.com>
2024-11-11 15:12:21 +0530

    1

--- devops-poe/src/main/java/com/example/Main.java
+++ devops-poe/src/main/java/com/example/Main.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is for my DevOps POE");
+        System.out.println("This is the DevOps Practical Oral Examination");
     }
 }
